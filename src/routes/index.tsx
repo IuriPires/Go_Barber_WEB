a98da03fe7095d52de58db2500bcd72be6e3a837
Iuri Pires <iuripires.work@gmail.com>
2020-08-15 15:08:06 -0300

    .

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,6 +5,8 @@ import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 import Dashboard from '../pages/Dashboard';
 
+console.log(Dashboard);
+
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SignIn} />
